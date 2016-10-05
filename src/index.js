84011f2c522cf91ed0858408799e5735b62b2ee8
Kyhan Turner <kyhanpt@gmail.com>
2016-10-05 15:24:22 -0700

    throttle api calls from user input

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import _ from 'lodash';
 import YTSearch from 'youtube-api-search';
 import VideoList from './components/video_list';
 import SearchBar from './components/search_bar';
@@ -30,9 +31,10 @@ class App extends Component {
 	}
 
 	render() {
+		const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
 		return (
 			<div>
-				<SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+				<SearchBar onSearchTermChange={videoSearch} />
 				<VideoDetail video={this.state.selectedVideo} />
 				<VideoList 
 					videos={this.state.videos}
