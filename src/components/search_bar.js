e7283d6ac9299676d88b62107cda2be17203db0f
Kyhan Turner <kyhanpt@gmail.com>
2016-10-05 12:14:27 -0700

    search bar component rendered to DOM

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,1 +1,15 @@
-search_bar.js
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {term: ''}
+	}
+
+	render() {
+		return <input onChange={event => this.setState({term: event.target.value})} />
+	}
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyBfSP7R70w6pMMz39HmjJ7uQQa2plLnrTE'
+
 const App = () => {
-	return <div>Some text</div>
+	return (
+		<div>
+			<SearchBar />
+		</div>
+	);
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'))
