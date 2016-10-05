79efb68880b6411ad10a569f22824144f112fb49
Kyhan Turner <kyhanpt@gmail.com>
2016-10-05 13:30:36 -0700

    restructured App component from functional to class based, set up YT API call

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyBfSP7R70w6pMMz39HmjJ7uQQa2plLnrTE'
 
-const App = () => {
-	return (
-		<div>
-			<SearchBar />
-		</div>
-	);
-}
+class App extends Component {
+	constructor(props){
+		super(props)
+
+		this.state = {videos: []};
+
+		YTSearch({key: API_KEY, term: 'Ron Swanson'}, videos => {
+			this.setState({videos})
+		}){
+
+		}
+	}
+	render() {
+		return (
+			<div>
+				<SearchBar />
+			</div>
+		);
+	}
+}	
 
 ReactDOM.render(<App />, document.querySelector('.container'))
