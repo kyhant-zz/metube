823ace98c2799096b195b14b8cd39dcf7837225d
Kyhan Turner <kyhanpt@gmail.com>
2016-10-05 14:23:41 -0700

    video previews rendering to DOM

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
+import VideoList from './components/video_list';
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyBfSP7R70w6pMMz39HmjJ7uQQa2plLnrTE'
+
+
 
 class App extends Component {
 	constructor(props){
@@ -12,16 +13,15 @@ class App extends Component {
 
 		this.state = {videos: []};
 
-		YTSearch({key: API_KEY, term: 'Ron Swanson'}, videos => {
+		YTSearch({key: API_KEY, term: 'Ron Swanson'}, (videos) => {
 			this.setState({videos})
-		}){
-
-		}
+		})
 	}
 	render() {
 		return (
 			<div>
 				<SearchBar />
+				<VideoList videos={this.state.videos} />
 			</div>
 		);
 	}
