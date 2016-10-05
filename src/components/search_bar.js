653a7f1eb29472205e6e83c869f3642bc9d132fe
Kyhan Turner <kyhanpt@gmail.com>
2016-10-05 15:17:48 -0700

    can search for new videos

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -8,7 +8,18 @@ class SearchBar extends Component {
 	}
 
 	render() {
-		return <input onChange={event => this.setState({term: event.target.value})} />
+		return (
+			<div className="search-bar">
+				<input 
+					value={this.state.term}
+					onChange={event => this.onInputChange(event.target.value)} />
+			</div>
+		)
+	}
+
+	onInputChange(term){
+		this.setState({term})
+		this.props.onSearchTermChange(term)
 	}
 }
 
